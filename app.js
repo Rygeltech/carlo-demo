27c751812df78469e4f1f96b9f8d0a70fe31f5fc
Justine Taglucop <justine.sugbodoc@gmail.com>
2022-09-12 15:07:38 +0800

    Add DOM

---
+++ app.js
@@ -0,0 +1,9 @@
+const myName = "Justine Taglucop";
+const address = "Tabok Lamac Yati Lilo-an";
+
+const addName = document.getElementById("myName");
+const addAddress = document.getElementById("address");
+
+addName.innerText = myName;
+
+addAddress.innerText = address;
